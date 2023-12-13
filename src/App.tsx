a2d14fef70c6afee1a3b6f6b1e17d53efad959be
Prasanthisandaka2005 <prasanthisandaka2005@gmail.com>
2023-12-13 19:57:35 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,96 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import SingleCard from './components/SingleCard';
 
-function App() {
-
-
-  return (
-    <>
-      <h1>Memory Cards Game
-      </h1>
-    </>
-  )
+interface Card {
+  src: string;
+  matched: boolean;
+  id: number;
 }
 
-export default App
+const cardImages: Card[] = [
+  { src: '../public/img/gem5.jpg', matched: false, id: Math.random() },
+  { src: '../public/img/gem4.jpg', matched: false, id: Math.random() },
+  { src: '../public/img/gem2.png', matched: false, id: Math.random() },
+  { src: '../public/img/gem3.jpg', matched: false, id: Math.random() },
+  { src: '../public/img/gem6.png', matched: false, id: Math.random() },
+  { src: '../public/img/gem1.webp', matched: false, id: Math.random() }
+
+];
+
+const App: React.FC = () => {
+  const [cards, setCards] = useState<Card[]>([]);
+  const [turns, setTurns] = useState<number>(0);
+  const [choiceOne, setChoiceOne] = useState<Card | null>(null);
+  const [choiceTwo, setChoiceTwo] = useState<Card | null>(null);
+  const [disabled, setDisabled] = useState<boolean>(false);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setChoiceOne(null);
+    setChoiceTwo(null);
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  const handleChoice = (card: Card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  return (
+    <div>
+      <div className='content-h'>
+      <h1>Memory Cards Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      </div>
+      <div className='card-wrapper'>
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          ></SingleCard>
+        ))}</div>
+      <p className='turns'>Turns : {turns}</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SingleCard.tsx
@@ -0,0 +1,29 @@
+import './singleCard.css';
+
+interface SingleCardProps {
+  card: {
+    src: string;
+    matched: boolean;
+    id: number;
+  };
+  handleChoice: (card: { src: string; matched: boolean; id: number }) => void;
+  flipped: boolean;
+  disabled: boolean;
+}
+
+export default function SingleCard({ card, handleChoice, flipped, disabled }: SingleCardProps) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className='card'>
+      <div className={flipped ? 'flipped' : ''}>
+        <img src={card.src} className='front' alt='card front' />
+        <img src='../public/img/coverImg.jpg' className='back' onClick={handleClick} alt='card back' />
+      </div>
+    </div>
+  );
+}
